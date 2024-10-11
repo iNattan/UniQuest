156cb412d81bac3d203d406bc881dfd53f2c0421
iNattan <natanmart09@gmail.com>
2024-10-10 23:41:58 -0300

    Tentativa de ajustar middleware

--- src/http/middlewares/verify-user-role.ts
+++ src/http/middlewares/verify-user-role.ts
@@ -2,7 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 
 export function verifyUserRole(roleToVerify: 0 | 1) {
   return (request: FastifyRequest, reply: FastifyReply) => {
-    const role = request.user.role
+    const { role } = request.user
 
     if (role !== roleToVerify) {
       return reply.status(401).send({ message: 'Unauthorized.' })
